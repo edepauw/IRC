91afa95628515418c9166c4a3df6f2940d854f95
Axel Lidy <alidy@z3r11p3.42lyon.fr>
2021-07-20 10:31:40 +0200

    +sever.hpp

--- test/server.cpp
+++ test/server.cpp
@@ -1,6 +1,6 @@
 #include "Server.hpp"
 
-Server::Server(): _n_user(0), _port(SERVER_PORT)
+Server::Server(): _n_user(0)
 {
 
 }
@@ -13,7 +13,7 @@ Server::~Server()
 void Server::addUser(int fd)
 {
 	User usr(fd);
-	_user.insert({fd, usr});
+	_user[fd] = usr;
 	_n_user++;
 }
 
@@ -25,4 +25,132 @@ void Server::setPort(int port)
 void Server::setPassword(std::string password)
 {
 	_password = password;
+}
+
+int	Server::getPort(void)
+{
+	return (_port);
+}
+
+std::string	Server::getPassword(void)
+{
+	return (_password);
+}
+
+
+void	Server::init(void)
+{  
+   if ((this->_data.master_socket = socket(AF_INET, SOCK_STREAM, 0)) <= 0)
+   {
+      perror("socket() failed");
+      exit(EXIT_FAILURE);
+   }
+   if (setsockopt(this->_data.master_socket, SOL_SOCKET,  SO_REUSEADDR, (char *)&_data.opt, sizeof(_data.opt)) < 0)
+   {
+      perror("setsockopt() failed");
+      close(_data.master_socket);
+      exit(EXIT_FAILURE);
+   }
+   fcntl(this->_data.master_socket, F_SETFL, O_NONBLOCK);
+   memset(&_data.addr, 0, sizeof(_data.addr));
+   _data.addr.sin_family      = AF_INET;
+   _data.addr.sin_addr.s_addr = htonl(INADDR_ANY);
+   _data.addr.sin_port        = htons(_port);
+   _data.addrlen = sizeof(_data.addr);
+   if(bind(_data.master_socket, reinterpret_cast<struct sockaddr*>(&_data.addr), _data.addrlen) < 0)
+   {
+      perror("bind() failed");
+      close(_data.master_socket);
+      exit(EXIT_FAILURE);
+   }
+   if ((listen(_data.master_socket, 3) < 0))
+   {
+      perror("listen() failed");
+      close(_data.master_socket);
+      exit(EXIT_FAILURE);
+   }
+   FD_ZERO(&_data.readfds);
+   FD_SET(_data.master_socket, &_data.readfds);
+   _user[_data.master_socket];
+   _data.max_sd = _data.master_socket;
+}
+
+void Server::loop(void)
+{
+    do
+   {
+      this->select_fun();
+      for (_data.it = _user.begin(); _data.it != _user.end(); _data.it++)
+      {
+         if (FD_ISSET(_data.it->first, &_data.readfds)) // masterset[i] = I/O , qui est set via select() plus haut a 1 si le fd a eu de l'activite
+         {
+            //desc_ready -= 1;
+            if (_data.it->first == _data.master_socket) // si i == listen_sd, activiter sur le fd du server, soit demande de connection
+            {
+               this->new_connection();
+            }
+            else // si i != listen_sd, il ne s'agis pas du server mais d'un client_fd en activiter
+            {
+               this->receive();
+            }
+        }
+      }
+
+    } while (TRUE);
+}
+
+void Server::select_fun(void)
+{
+   std::cout << "Waiting on select()..." << std::endl;
+      if ((_data.ret_select = select(_data.max_sd + 1, &_data.readfds, NULL, NULL, NULL)) < 0)
+      {
+         perror("  select() failed");
+         exit(EXIT_FAILURE);
+      }
+}
+
+void Server::new_connection(void)
+{
+   printf("  Listening socket is readable\n");
+   if ((_data.new_sd = accept(_data.master_socket, reinterpret_cast<struct sockaddr *>(&_data.addr), reinterpret_cast<socklen_t *>(&_data.addrlen))) < 0)
+   {
+      if (errno != EWOULDBLOCK)
+         perror("  accept() failed");
+      exit(EXIT_FAILURE);
+   }
+   printf("  New incoming connection - %d\n", _data.new_sd);
+   FD_SET(_data.new_sd, &_data.readfds);
+   addUser(_data.new_sd);
+   if (_data.new_sd > _data.max_sd)
+      _data.max_sd = _data.new_sd;
+}
+
+void Server::receive(void)
+{
+   printf("  Descriptor %d is readable\n", _data.it->first);
+    if((_data.ret_read = recv(_data.it->first, _data.buffer, sizeof(_data.buffer), 0)) < 0)
+   {
+      perror("recv() failed");
+      exit(EXIT_FAILURE);
+   }
+   if (_data.ret_read == 0)
+   {
+       _user.erase(_data.it->first);
+       close(_data.it->first);
+       FD_CLR(_data.it->first, &_data.readfds);
+       if (_data.it->first == _data.max_sd)
+       {
+          while (FD_ISSET(_data.max_sd, &_data.readfds) == FALSE)
+             _data.max_sd -= 1;
+       }
+   }
+    //serv.parseMsg(//i ,buffer, serv);
+    //serv.clearBuffer();
+}
+
+void Server::close_con(void)
+{
+   for (_data.it = _user.begin(); _data.it != _user.end(); _data.it++)
+      if (FD_ISSET(_data.it->first, &_data.readfds))
+         close(_data.it->first);
 }
--- test/server.hpp
+++ test/server.hpp
@@ -1,35 +1,78 @@
 #ifndef SERVER_HPP
 # define SERVER_HPP
 # include <map>
+# include <list>
 # include <iostream>
 # include <string>
-# include <stdio.h>
-# include <unistd.h>
 # include <cstring>
 # include <iostream>
 # include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <cerrno>
+# include <fcntl.h>
 # include <sys/ioctl.h>
 # include <sys/socket.h>
+# include <sys/types.h>
+# include <sys/select.h>
 # include <sys/time.h>
 # include <netinet/in.h>
-# include <errno.h>
-# include <fcntl.h>
-# define SERVER_PORT  12345
+# include "User.hpp"
+
+#define TRUE 1
+#define FALSE 0
+
+typedef struct s_data
+{
+    struct sockaddr_in   addr;
+    int                  master_socket;
+    int                  new_sd;
+    int                  ret_select;
+    int                  ret_read;
+    char*                buffer[1025];
+    fd_set               readfds;
+    int                  opt;
+    struct timeval       timeout;
+    int                  addrlen;
+    int                  max_sd;
+    std::map<int, User>::iterator it;
+
+    s_data(){opt = TRUE;};
+}               t_data;
 
 class Server
 {
     public:
         Server();
         ~Server();
+
         void setPort(int port);
         void setPassword(std::string password);
         void addUser(int fd);
-        void start();
+
+        int         getPort(void);
+        std::string getPassword(void);
+
+
+        void init(void);
+        void loop(void);
+        void close_con(void);
+
     private:
-        std::map<int, User> _user;
-        int _n_user;
-        int _port;
-        std::string _password;
+        std::map<int, User>                     _user;
+        std::map<std::string, std::list<int> >  _chan;
+        int                                     _n_user;
+        int                                     _port;
+        std::string                             _password;
+        t_data                                  _data;
+
+        void clearBuffer(void);
+        void new_connection(void);
+        void receive(void);
+        void select_fun(void);
+
+    protected:
+
 };
 
 #endif
